bd08ca17f1cc45e08ee9012496db6b7f093ffad7
arbuztw <arbuz.tw@gmail.com>
2013-09-15 11:52:32 +0800

    Add "Expense Add" function

--- book/urls.py
+++ book/urls.py
@@ -3,4 +3,5 @@ from views import *
 
 urlpatterns = patterns('book.views',
 	url(r'^exp/(\d+)/(\d+)/(\d+)$', ExpShow.as_view()),
+	url(r'exp/(\d+)/(\d+)/(\d+)/add$', ExpAdd.as_view()),
 )
--- book/views.py
+++ book/views.py
@@ -24,3 +24,40 @@ class ExpShow(View):
 			'credit': credit,
 			'categ': categ
 		})
+
+class ExpAdd(View):
+	def post(self, request, year, month, day):
+		year = int(year)
+		month = int(month)
+		day = int(day)
+		try:
+			d = date(year, month, day)
+		except:
+			raise Http404
+
+		data = {
+			'name': request.POST['name'],
+			'amount': int(request.POST['amount']),
+			'categ': int(request.POST['categ']),
+			'pay': int(request.POST['pay'])
+		}
+
+		exp = Exp(date=d, categ_id=data['categ'], name=data['name'], amount=data['amount'], pay_id=data['pay'])
+		exp.save()
+
+		if data['pay'] == 1:
+			p = Monthly.objects.get(year=year, month=month)
+			p.exp += data['amount']
+			p.save()
+		else:
+			p = Credit.objects.get(id=data['pay'])
+			l = map(lambda x: int(x), p.amout.split(','))
+			l[month-1] += data['amount']
+			p.amount = ','.join(map(lambda x:str(x), l))
+			p.save()
+
+			p = Monthly.objects.get(year=year, month=month+1)
+			p.credit += data['amount']
+			p.save()
+
+		return render(request, 'exp_add.html', {'exp': exp})
